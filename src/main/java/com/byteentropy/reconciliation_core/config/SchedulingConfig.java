068892bad396a7cc40aa40044bc57d168841bdea
ByteEntropy <34189432+ByteEntropyDotCom@users.noreply.github.com>
2026-04-29 15:59:55 +0800

    Update and rename SchedulingConfig.java to SchedulingConfig.java

--- src/main/java/com/byteentropy/reconciliation_core/config/SchedulingConfig.java
+++ src/main/java/com/byteentropy/reconciliation_core/config/SchedulingConfig.java
@@ -1,4 +1,4 @@
-package com.byteentropy.event_core.config;
+package com.byteentropy.reconciliation_core.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
